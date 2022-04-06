f23b0500007c83d9daf929484c62c41fb65f4996
waiga <hyang9387@gmail.com>
2022-04-06 18:08:30 +0900

    Initial Commit

---
+++ Shared/NotesMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesMacOSApp.swift
+//  Shared
+//
+//  Created by 김미향 on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct NotesMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
